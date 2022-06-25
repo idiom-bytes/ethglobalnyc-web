3a9411851c044dea5d74b45836696a746fd71d0c
Sheldon Trotman <shel.trotman@gmail.com>
2022-06-25 04:58:24 -0400

    Set up the base pages now to fill them in with more detailed componenets

--- src/pages/login/LogIn.tsx
+++ src/pages/login/LogIn.tsx
@@ -2,16 +2,14 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { Link } from "react-router-dom";
 
 import coverOne from "../../assets/images/cover-4.jpg";
 
@@ -47,15 +45,26 @@ export default function SignInSide() {
             backgroundPosition: "center",
           }}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <Box
             sx={{
-              my: 8,
               mx: 4,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              mt: "calc(50% - 52px)",
             }}
           >
             <Avatar sx={{ m: 2 }}>
@@ -67,14 +76,11 @@ export default function SignInSide() {
             </Typography>
 
             <Box>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Log In
-              </Button>
+              <Link to={"/apps"}>
+                <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                  Log In
+                </Button>
+              </Link>
             </Box>
           </Box>
         </Grid>
--- src/router.tsx
+++ src/router.tsx
@@ -4,33 +4,25 @@ import "./App.css";
 
 // Pre auth routes
 import Login from "./pages/login/LogIn";
+import CatchAll from "./pages/catch/App";
 
 // Post auth routes
-import Transactions from "./pages/transactions/App";
-import CatchAll from "./pages/catch/App";
+import Apps from "./pages/apps/App";
+import Contracts from "./pages/contracts/App";
+import Crm from "./pages/crm/App";
 import Dashboard from "./pages/dashboard/Dashboard";
+import Transactions from "./pages/transactions/App";
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/app">
-        <Dashboard />
-      </Route>
-      <Route path="/app/:API_ID/crm">
-        <Dashboard />
-      </Route>
-      <Route path="/app/:API_ID/contracts">
-        <Dashboard />
-      </Route>
-      <Route path="/app/:API_ID/transactions">
-        <Transactions />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="*">
-        <CatchAll />
-      </Route>
+      <Route path="/app/:API_ID/crm" element={<Crm />} />
+      <Route path="/app/:API_ID/contracts" element={<Contracts />} />
+      <Route path="/app/:API_ID/transactions" element={<Transactions />} />
+      <Route path="/app/:API_ID" element={<Dashboard />} />
+      <Route path="/apps" element={<Apps />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<CatchAll />} />
     </Routes>
   );
 }
